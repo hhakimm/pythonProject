b9e3d96abe39709a8f6f8575065b4c29d52c0c4f
hhakimm <hadihakim00@icloud.com>
2021-07-22 09:18:10 +0800

    test 2

---
+++ demoVcs.py
@@ -0,0 +1,19 @@
+# Program make a simple calculator
+num1 = 50
+num2 = 40
+# This function adds two numbers
+def add(x, y):
+    return num1 + num2
+print(num1+num2)
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return num1 - num2
+print(num1-num2)
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return num1 * num2
+print(num1*num2)
+
+
